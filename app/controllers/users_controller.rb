e39e85ee15865d1d4e413145a9169e38310fcbac
vibhu0100 <57134712+vibhu0100@users.noreply.github.com>
2021-03-03 17:32:28 +0530

    Add files via upload

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+    before_action :authorized, only: [:auto_login]
+
+    def create
+        @user=User.new(user_params)
+        if @user.save
+            token=encode_token({user_id: @user.id})
+            render json: {user: @user, token: token}
+        else
+            render json: {error: "Enter Proper data"}
+        end
+
+
+    end
+    def authenticate
+        @user=User.find_by(email: params[:email])
+
+        if @user.password.eql?(params[:password])
+            token=encode_token({user_id: @user.id})
+            render json: {user: @user, token: token}
+        else
+            render json: {error: "Invalid"}
+        end
+=begin
+        if user
+            if user.password==params[:password]
+                render json: "Authenticted"
+            else
+                render json: "Enter correct password"
+            end
+        else
+            render json: "You are not registered"
+        end
+=end
+
+
+    end
+    def auto_login
+        render json: @user
+    end
+
+    private
+    def user_params
+        params.permit(:name,:email,:password)
+    end
+
+end
