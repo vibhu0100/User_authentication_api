5b8ac9ff634eadb06aa7a94695b25afa79886467
vibhu0100 <57134712+vibhu0100@users.noreply.github.com>
2021-03-04 14:32:46 +0530

    Formatted Code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,28 @@
 class UsersController < ApplicationController
-    before_action :authorized, only: [:auto_login]
-    
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            token = encode_token({user_id: @user.id})
-            render json: {user: @user, token: token}
-        else
-            render json: {error: "Enter Proper data"}
-        end
-    end
-    def authenticate
-        @user = User.find_by(email: params[:email])
-        if @user.password.eql?(params[:password])
-            token = encode_token({user_id: @user.id})
-            render json: {user: @user, token: token}
-        else
-            render json: {error: "Invalid"}
-        end
-    end
-    def auto_login
-        render json: @user
-    end
-    private
-    def user_params
-        params.permit(:name,:email,:password)
-    end
+  before_action :authorized, only: [:auto_login]
+  def create
+    @user = User.new(user_params)
+      if @user.save
+          token = encode_token({user_id: @user.id})
+          render json: {user: @user, token: token}
+      else
+          render json: {error: "Enter Proper data"}
+      end
+  end
+  def authenticate
+      @user = User.find_by(email: params[:email])
+      if @user.password.eql?(params[:password])
+        token = encode_token({user_id: @user.id})
+        render json: {user: @user, token: token}
+      else
+        render json: {error: "Invalid"}
+      end
+  end
+  def auto_login
+    render json: @user
+  end
+  private
+  def user_params
+    params.permit(:name,:email,:password)
+  end
 end
