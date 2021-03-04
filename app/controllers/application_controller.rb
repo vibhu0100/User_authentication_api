77929e6da0abfeb8b97e71dc5b3154046158c262
vibhu0100 <57134712+vibhu0100@users.noreply.github.com>
2021-03-04 14:09:17 +0530

    Formatted code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,45 +2,32 @@ class ApplicationController < ActionController::API
     before_action :authorized
 
     def authorized
-        render json: {message: "Please log in"},status: :unauthorized unless logged_in?
+        render json: {message: "Please log in"}, status: :unauthorized unless logged_in?
+        end
     end
     def logged_in?
-    !!logged_in_user
+        !!logged_in_user
     end
-
-
     def logged_in_user
-    if decoded_token
-        user_id=decoded_token[0]['user_id']
-        @user=User.find_by(id: user_id)
+        if decoded_token
+            user_id = decoded_token[0]['user_id']
+            @user = User.find_by(id: user_id)
+        end
     end
-end
-
-
-    def decodeds_token
+    def decoded_token
         if auth_header
-            token=auth_header.split(' ')[1]
+            token = auth_header.split(' ')[1]
             begin
-                JWT.decode(token, 'secret',true,algorith: 'HS256')
-
+                JWT.decode(token, 'secret', true, algorith: 'HS256')
             rescue JWT::DecodeError
                 nil
-
-                
             end
         end
-
     end
-
-    def encode_token(payload)
-    JWT.encode(payload, 'secret')
-  end
-
-  def auth_header
-    # { Authorization: 'Bearer <token>' }
-    request.headers['Authorization']
-  end
-
-
-
+  def encode_token(payload)
+        JWT.encode(payload, 'secret')
+    end
+    def auth_header
+        request.headers['Authorization']
+    end
 end
