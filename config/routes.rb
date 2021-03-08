684d21c427ec86e84bb6483f6944551afc235475
Vibhuti Patel <vibhutipatel412@gmail.com>
2021-03-08 17:48:37 +0530

    resolved

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'signup', to: 'users#create'
-<<<<<<< Updated upstream
-  post 'authenticate', to: 'users#authenticate' 
-end
-=======
   post 'login', to: 'users#login'
   post 'send_otp', to: 'users#send_otp'
   post 'verify_otp', to: 'users#verify'
@@ -15,6 +11,4 @@ end
   get 'my_posts', to: 'post#index'
   post 'edit_post', to: 'post#update'
   get 'delete_post/:id', to: 'post#delete'
-  post 'otp', to: 'otp#create'
 end 
->>>>>>> Stashed changes
