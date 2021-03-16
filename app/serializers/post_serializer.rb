d0c9f00fb093ce0ec48b43798aa72e14d634d1aa
Vibhuti Patel <vibhutipatel412@gmail.com>
2021-03-16 12:11:54 +0530

    added comment feature

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,6 +1,17 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :owner, :caption, :images, :created_at, :updated_at
+  include ActionView::Helpers::DateHelper
+  include ActiveStorage::Blobs
+  Rails.application.routes.url_helpers
+  has_many :comments
+  attributes :id, :owner, :caption, :images, :created, :image
   def owner
     User.find(object.user_id).name
+  end
+  def image
+    object.images.size()
+  end
+  
+  def created
+    distance_of_time_in_words(object.created_at, Time.now) + " ago"
   end
 end
